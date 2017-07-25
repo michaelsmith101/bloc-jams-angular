d82cffcb2a0af78484ed967849d1eee8e5c042d7
mts mts <michaelsmith101@gmail.com>
2017-07-25 10:55:04 -0700

    Working Pause and Play buttons complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,7 +1,9 @@
 (function() {
+
     angular
       .module('blocJams')
-      .controller('AlbumCtrl', ["$scope", 'Fixtures', function($scope, Fixtures){
+      .controller('AlbumCtrl', ["$scope", 'Fixtures', 'SongPlayer', function($scope, Fixtures, SongPlayer){
            $scope.albumData = Fixtures.getAlbum();
+           $scope.songPlayer = SongPlayer;
       }]);
   })();
